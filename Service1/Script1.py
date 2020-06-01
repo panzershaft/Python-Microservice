ecc832921b805560c7275d5788530d8fc234213d
Soham <contractor.soham@gmail.com>
2020-06-01 02:47:04 +0100

    Service1 done, tested locally, but shell is designed for the bigger part

---
+++ Service1/Script1.py
@@ -0,0 +1,46 @@
+"""
+This Python script will do the following:
+
+1.Take JSON as a input, if not specified will use the default json
+2.Convert the JSON to XML
+3.Encrypt the XML
+4.Transfer to a remote location
+"""
+import json as JS
+import xml.etree.cElementTree as Et
+from xml.etree import ElementTree
+
+from cryptography.fernet import Fernet
+from dicttoxml import dicttoxml
+
+
+def script_runner(file_name="default_json.json"):
+    with open(file_name) as json_format_file:
+        json_data = JS.load(json_format_file)
+
+    xml_obj = dicttoxml(json_data, custom_root='test', attr_type=False)
+    xml_string = ElementTree.fromstring(xml_obj)
+    final_xml = Et.ElementTree(xml_string)
+    final_xml.write("plain_xml.xml")
+
+    new_key = Fernet.generate_key()
+    with open("secret_key.key", "wb") as key_file:
+        key_file.write(new_key)
+
+    with open("plain_xml.xml", "rb") as file:
+        # read all file data
+        file_data = file.read()
+    key = open("secret_key.key", "rb").read()
+    f = Fernet(key)
+    encrypted_data = f.encrypt(file_data)
+
+    with open("encrypted_xml.xml", "wb") as file:
+        file.write(encrypted_data)
+
+
+if __name__ == '__main__':
+    script_runner()
+# while True:
+#         print("This prints once a minute.")
+#         time.sleep(600000)
+print("It ran?")
---
+++ Service1/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 Script1.py
+mv secret_key.key encrypted_xml.xml /data_volume1/
+exit
